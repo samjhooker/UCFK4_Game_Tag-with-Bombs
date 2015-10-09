ac6a697762064758d66256f734192b5bad2babce
samjhooker <samjhooker@gmail.com>
2015-10-09 21:07:08 +1300

    added inline comments and more documentation to make the code easy for anyone to understand

--- lab3-ex4.c
+++ lab3-ex4.c
@@ -31,11 +31,11 @@ int lose = 0;
 Struct defining the stored properties for a single character instance
 */
 typedef struct {
-    int x;
-    int y;
-    int hasBomb;
-    int prevX;
-    int prevY;
+		int x;
+		int y;
+		int hasBomb;
+		int prevX;
+		int prevY;
 } Character;
 
 
@@ -48,10 +48,10 @@ returns: integer representation of the given string
 */
 int stringToInt(char* str, int bombsActive){
 	str++;
-    int result = 0;
-    int i;
-    for (i = 0; i < 7; i++)
-    {
+		int result = 0;
+		int i;
+		for (i = 0; i < 7; i++)
+		{
 		result = result << 1;
 		if (bombsActive == 0){
 			result += ((*str != '0') && (*str != 'a') && (*str != 'b')&& (*str != 'c')&& (*str != 'd')&& (*str != 'f')&& (*str != 'g')&& (*str != 'h')&& (*str != 'i')&& (*str != 'j'));
@@ -68,34 +68,34 @@ int stringToInt(char* str, int bombsActive){
 
 // the main gameboard used for the application
 char* matrix[] ={"eeeeeeeee",
-				  "eaaaaaaae",
-				  "ebbbbbbbe",
-				  "eccccccce",
-				  "eddddddde",
-				  "efffffffe",
-				  //screen two starts here
-				  "eggggggge",
-				  "ehhhhhhhe",
-				  "e0001000e",
-				  "eiiiiiiie",
-				  "ejjjjjjje",
-				  "eeeeeeeee"};
+					"eaaaaaaae",
+					"ebbbbbbbe",
+					"eccccccce",
+					"eddddddde",
+					"efffffffe",
+					//screen two starts here
+					"eggggggge",
+					"ehhhhhhhe",
+					"e0001000e",
+					"eiiiiiiie",
+					"ejjjjjjje",
+					"eeeeeeeee"};
 
 
 // list of all pins driving rows in the LED matrix
 static const pio_t rows[] =
 {
-    LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
-    LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
-    LEDMAT_ROW7_PIO
+		LEDMAT_ROW1_PIO, LEDMAT_ROW2_PIO, LEDMAT_ROW3_PIO,
+		LEDMAT_ROW4_PIO, LEDMAT_ROW5_PIO, LEDMAT_ROW6_PIO,
+		LEDMAT_ROW7_PIO
 };
 
 
 // list of all pins driving columns in the LED matrix
 static const pio_t cols[] =
 {
-    LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
-    LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
+		LEDMAT_COL1_PIO, LEDMAT_COL2_PIO, LEDMAT_COL3_PIO,
+		LEDMAT_COL4_PIO, LEDMAT_COL5_PIO
 };
 
 
@@ -113,8 +113,8 @@ static void display_column (uint8_t row_pattern, uint8_t current_column)
 
 	pio_output_high(LEDMAT_ROW1_PIO);
 	pio_output_high(LEDMAT_ROW2_PIO);
-  pio_output_high(LEDMAT_ROW3_PIO);
-  pio_output_high(LEDMAT_ROW4_PIO);
+	pio_output_high(LEDMAT_ROW3_PIO);
+	pio_output_high(LEDMAT_ROW4_PIO);
 	pio_output_high(LEDMAT_ROW5_PIO);
 	pio_output_high(LEDMAT_ROW6_PIO);
 	pio_output_high(LEDMAT_ROW7_PIO);
@@ -153,21 +153,21 @@ y: the y location of where to start the explosion
 frame: the frame (1-3) of the animation to be applied
 */
 void showExplosion(int x, int y, int frame){
-  if(frame == 1){
-  matrix[x+1][y+1] = 'w';
-  matrix[x-1][y-1] = 'w';
-  matrix[x+1][y-1] = 'w';
-  matrix[x-1][y+1] = 'w';
+	if(frame == 1){
+	matrix[x+1][y+1] = 'w'; //frame 1
+	matrix[x-1][y-1] = 'w';
+	matrix[x+1][y-1] = 'w';
+	matrix[x-1][y+1] = 'w';
 }else if (frame ==2){
-  matrix[x+2][y+0] = 'w';
-  matrix[x-2][y-0] = 'w';
-  matrix[x+0][y-2] = 'w';
-  matrix[x-0][y+2] = 'w';
+	matrix[x+2][y+0] = 'w'; //frame 2
+	matrix[x-2][y-0] = 'w';
+	matrix[x+0][y-2] = 'w';
+	matrix[x-0][y+2] = 'w';
 }else{
-  matrix[x+3][y+3] = 'w';
-  matrix[x-3][y-3] = 'w';
-  matrix[x+3][y-3] = 'w';
-  matrix[x-3][y+3] = 'w';
+	matrix[x+3][y+3] = 'w'; //frame 3
+	matrix[x-3][y-3] = 'w';
+	matrix[x+3][y-3] = 'w';
+	matrix[x-3][y+3] = 'w';
 }
 }
 
@@ -178,320 +178,392 @@ x: the x position to check for a win/lose
 y: the y position to check for a win/lose
 */
 void checkForWinOrLose(int x, int y){
-  if ((win == 0) && (lose ==0)){
-  if (matrix[x][y] == '1') { //hit other player
-    win = 1;
-    ir_uart_putc ('L');
-  }else if (matrix[x][y] == 'x'){ //hit a bomb
-    lose = 1;
-    ir_uart_putc ('W');
-  }
+	if ((win == 0) && (lose ==0)){
+	if (matrix[x][y] == '1') { //hit other player
+		win = 1;
+		ir_uart_putc ('L');
+	}else if (matrix[x][y] == 'x'){ //hit a bomb
+		lose = 1;
+		ir_uart_putc ('W');
+	}
 }
 
 }
 
+
+
+/*
+Moves a player in the matrix north by one position. transmits the location to the other MCU if required
+player: pointer to the Character to move
+transmit: boolean indicating whether the move will be transmitted to the other MCU via IR.
+*/
 void goNorth(Character *player, int transmit){
 
-  if (matrix[player->x][player->y + 1] != 'e'){
+	if (matrix[player->x][player->y + 1] != 'e'){
 
 
 
-    player->prevX = player->x;
-    player->prevY = player->y;
+		player->prevX = player->x;
+		player->prevY = player->y;
 
-    matrix[player->x][player->y] = '0';
-    player->y++;
-    checkForWinOrLose(player->x, player->y);
-    matrix[player->x][player->y] = '1';
+		matrix[player->x][player->y] = '0';
+		player->y++;
+		checkForWinOrLose(player->x, player->y);
+		matrix[player->x][player->y] = '1';
 
 
-    if (transmit == 1){
-      ir_uart_putc ('n');
-    }
+		if (transmit == 1){
+			ir_uart_putc ('n');
+		}
 
-  }
+	}
 
 }
+
+
+/*
+Moves a player in the matrix south by one position. transmits the location to the other MCU if required
+player: pointer to the Character to move
+transmit: boolean indicating whether the move will be transmitted to the other MCU via IR.
+*/
 void goSouth(Character *player, int transmit){
 
-  if (matrix[player->x][player->y - 1] != 'e'){
+	if (matrix[player->x][player->y - 1] != 'e'){
 
-    player->prevX = player->x;
-    player->prevY = player->y;
+		player->prevX = player->x;
+		player->prevY = player->y;
 
-    matrix[player->x][player->y] = '0';
-    player->y--;
-    checkForWinOrLose(player->x, player->y);
-    matrix[player->x][player->y] = '1';
+		matrix[player->x][player->y] = '0';
+		player->y--;
+		checkForWinOrLose(player->x, player->y);
+		matrix[player->x][player->y] = '1';
 
 
-    if (transmit == 1){
-      ir_uart_putc ('s');
-    }
+		if (transmit == 1){
+			ir_uart_putc ('s');
+		}
 
-  }
+	}
 
 }
+
+
+/*
+Moves a player in the matrix north by east position. transmits the location to the other MCU if required
+player: pointer to the Character to move
+transmit: boolean indicating whether the move will be transmitted to the other MCU via IR.
+*/
 void goEast(Character *player, int transmit){
 
-  if (matrix[player->x+1][player->y] != 'e'){
+	if (matrix[player->x+1][player->y] != 'e'){
 
-    player->prevX = player->x;
-    player->prevY = player->y;
+		player->prevX = player->x;
+		player->prevY = player->y;
 
-    matrix[player->x][player->y] = '0';
-    player->x++;
-    checkForWinOrLose(player->x, player->y);
-    matrix[player->x][player->y] = '1';
+		matrix[player->x][player->y] = '0';
+		player->x++;
+		checkForWinOrLose(player->x, player->y);
+		matrix[player->x][player->y] = '1';
 
 
-    if (transmit == 1){
-      ir_uart_putc ('e');
-    }
+		if (transmit == 1){
+			ir_uart_putc ('e');
+		}
 
-  }
+	}
 
 }
+
+
+/*
+Moves a player in the matrix west by one position. transmits the location to the other MCU if required
+player: pointer to the Character to move
+transmit: boolean indicating whether the move will be transmitted to the other MCU via IR.
+*/
 void goWest(Character *player, int transmit){
 
-  if (matrix[player->x-1][player->y] != 'e'){
+	if (matrix[player->x-1][player->y] != 'e'){
 
-    player->prevX = player->x;
-    player->prevY = player->y;
+		player->prevX = player->x;
+		player->prevY = player->y;
 
-    matrix[player->x][player->y] = '0';
-    player->x--;
-    checkForWinOrLose(player->x, player->y);
-    matrix[player->x][player->y] = '1';
+		matrix[player->x][player->y] = '0';
+		player->x--;
+		checkForWinOrLose(player->x, player->y);
+		matrix[player->x][player->y] = '1';
 
 
-    if (transmit == 1){
-      ir_uart_putc ('w');
-    }
+		if (transmit == 1){
+			ir_uart_putc ('w');
+		}
 
-  }
+	}
 
 }
 
-
+/*
+Places a bomb at the last know position of a character. transmits the location to the other MCU if required
+player: pointer to the Character to place bomb
+transmit: boolean indicating whether the bomb location will be transmitted to the other MCU via IR.
+*/
 void placeBomb(Character *player, int transmit){
 
-  matrix[player->prevX][player->prevY] = 'x';
-  if (transmit == 1){
-    ir_uart_putc ('b');
-  }
+	matrix[player->prevX][player->prevY] = 'x';
+	if (transmit == 1){
+		ir_uart_putc ('b');
+	}
 
 }
 
 
-
+/*
+shows the letter 'W' on the LED matrix. used for the winner
+*/
 void showWinScreen(){
-  while(1){
-    display_column (stringToInt("e0000000e", 1), 0);
-    display_column (stringToInt("e0101010e", 1), 1);
-    display_column (stringToInt("e0101010e", 1), 2);
-    display_column (stringToInt("e0010100e", 1), 3);
-    display_column (stringToInt("e0000000e", 1), 4);
-  }
+	while(1){
+		display_column (stringToInt("e0000000e", 1), 0);
+		display_column (stringToInt("e0101010e", 1), 1);
+		display_column (stringToInt("e0101010e", 1), 2);
+		display_column (stringToInt("e0010100e", 1), 3);
+		display_column (stringToInt("e0000000e", 1), 4);
+	}
 
 }
 
+/*
+shows the letter 'L' on the LED matrix, used for the loser
+*/
 void showLoseScreen(){
-  while(1){
-    display_column (stringToInt("e0010000e", 1), 0);
-    display_column (stringToInt("e0010000e", 1), 1);
-    display_column (stringToInt("e0010000e", 1), 2);
-    display_column (stringToInt("e0011100e", 1), 3);
-    display_column (stringToInt("e0000000e", 1), 4);
-  }
+	while(1){
+		display_column (stringToInt("e0010000e", 1), 0);
+		display_column (stringToInt("e0010000e", 1), 1);
+		display_column (stringToInt("e0010000e", 1), 2);
+		display_column (stringToInt("e0011100e", 1), 3);
+		display_column (stringToInt("e0000000e", 1), 4);
+	}
 
 }
 
+
+
+/*
+Main function, runs everything
+return: 0 as per c convention
+*/
 int main (void)
 {
 
-	int counter = 0;
+
+	//initiilize counters for lights
+	int counter = 0; 
 	int flashCounter = 0;
 	int flash = 1;
-  int explosionCounter = 0;
+	int explosionCounter = 0;
 
+
+	//intiilizes chatacters
 	Character player1;
 	player1.x = 8;
 	player1.y = 4;
-  player1.prevX = 8;
-	player1.prevY = 4;
+	player1.prevX = 8;
+	player1.prevY = 5;
 	player1.hasBomb = 0;
 
-  Character player2;
-  player2.x = 3;
-  player2.y = 4;
-  player2.prevX = 3;
-  player2.prevY = 4;
-  player2.hasBomb = 0;
+	Character player2;
+	player2.x = 3;
+	player2.y = 4;
+	player2.prevX = 3;
+	player2.prevY = 5;
+	player2.hasBomb = 0;
 
-	//int prevX;
-	//int prevY;
 
+	//initilize counters for the columns and the shown matrix columns
 	uint8_t current_column = 0;
 	int screen_index = 6;
 
-    system_init ();
-    //tinygl_init (PACER_RATE);
-    //tinygl_font_set (&font5x7_1);
-   // tinygl_text_speed_set (MESSAGE_RATE);
-    navswitch_init ();
-
-    /* TODO: Initialise IR driver.  */
+	//initilize system, navswitch and Infared
+	system_init ();
+	navswitch_init ();
 	ir_uart_init();
 
-    pacer_init (PACER_RATE);
-    pio_config_set (LED_PIO, PIO_OUTPUT_LOW);
+	//init the pacer and the LED light
+	pacer_init (PACER_RATE);
+	pio_config_set (LED_PIO, PIO_OUTPUT_LOW);
 
-    pio_config_set(LEDMAT_COL1_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_COL5_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_COL2_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_COL3_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_COL4_PIO, PIO_OUTPUT_HIGH);
-
-
-    pio_config_set(LEDMAT_ROW1_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW2_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW3_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW4_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW5_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW6_PIO, PIO_OUTPUT_HIGH);
-    pio_config_set(LEDMAT_ROW7_PIO, PIO_OUTPUT_HIGH);
-
-    while (1)
-    {
-
-		  counter ++;
-		  if (counter > (PACER_RATE * BOMB_TIME)){
-			     player1.hasBomb = 1;
-			   counter = 0;
-		  }
-		  flashCounter++;
-		      if (flashCounter > (PACER_RATE * BOMB_FLASH_TIME)){
-			      flash = 1-flash;
-			      flashCounter = 0;
-		}
-
-		display_column (stringToInt(matrix[screen_index], flash), current_column);
-
-        current_column++;
-        screen_index++;
-
-        if (current_column > (LEDMAT_COLS_NUM - 1))
-        {
-            current_column = 0;
-        }
-        if (screen_index > (10))
-        {
-            screen_index = 6;
-        }
+	//init all rows and columns of the LED matrix
+	pio_config_set(LEDMAT_COL1_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_COL5_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_COL2_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_COL3_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_COL4_PIO, PIO_OUTPUT_HIGH);
 
 
-        if (win > 0){
-          if (explosionCounter > (PACER_RATE * 0.15)){
-            if (win == 4){
-              break;
-            }
-            showExplosion(player2.x, player2.y, win);
-            explosionCounter = 0;
-            win++;
-          }
-          explosionCounter++;
-        }
-
-        if (lose > 0){
-          if (explosionCounter > (PACER_RATE * 0.15)){
-            if (lose == 4){
-              break;
-            }
-            showExplosion(player1.x, player1.y, lose);
-            explosionCounter = 0;
-            lose++;
-          }
-          explosionCounter++;
-        }
+	pio_config_set(LEDMAT_ROW1_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_ROW2_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_ROW3_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_ROW4_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_ROW5_PIO, PIO_OUTPUT_HIGH);
+	pio_config_set(LEDMAT_ROW6_PIO, PIO_OUTPUT_HIGH);
+  pio_config_set(LEDMAT_ROW7_PIO, PIO_OUTPUT_HIGH);
 
 
 
-        pacer_wait ();
-        //tinygl_update ();
-        navswitch_update ();
+  //loop while game is in operation
+	while (1)
+	{
 
 
-    if (navswitch_push_event_p (NAVSWITCH_NORTH))
+			//loop and counter to wait for the time for a bomb to spawn
+			counter ++;
+			if (counter > (PACER_RATE * BOMB_TIME)){
+				player1.hasBomb = 1;
+				counter = 0;
+			}
+
+			//loop and counter to wait for time for bombs to flash
+			flashCounter++;
+			if (flashCounter > (PACER_RATE * BOMB_FLASH_TIME)){
+				flash = 1-flash;
+				flashCounter = 0;
+			}
+
+			//displays a line in the game board matrix in a line in the LED matrix
+			display_column (stringToInt(matrix[screen_index], flash), current_column);
+
+				//incremnets these lines
+				current_column++;
+				screen_index++;
+
+				//and resets there if necessary
+				if (current_column > (LEDMAT_COLS_NUM - 1))
+				{
+						current_column = 0;
+				}
+				if (screen_index > (10))
+				{
+						screen_index = 6;
+				}
+
+
+				//if win, play win animation
+				if (win > 0){
+					if (explosionCounter > (PACER_RATE * 0.15)){
+						if (win == 4){ //three frames of animation
+							break; //breaks out of the game loop and ends the game
+						}
+						showExplosion(player2.x, player2.y, win);
+						explosionCounter = 0;
+						win++;
+					}
+					explosionCounter++;
+				}
+
+				//if lose, play lose animation
+				if (lose > 0){
+					if (explosionCounter > (PACER_RATE * 0.15)){
+						if (lose == 4){
+							break; //breaks out of the game loop and ends the game
+						}
+						showExplosion(player1.x, player1.y, lose);
+						explosionCounter = 0;
+						lose++;
+					}
+					explosionCounter++;
+				}
+
+
+				//waits for time to continue
+				pacer_wait ();
+			
+
+			//updates navswitch
+				navswitch_update ();
+
+
+
+		//listeners for the navswitches. 
+		//when pressed, calls the directional functions with a transmission of IR to the other MCU
+		if (navswitch_push_event_p (NAVSWITCH_NORTH))
 			goNorth(&player1, 1);
 
-    if (navswitch_push_event_p (NAVSWITCH_SOUTH))
-      goSouth(&player1, 1);
+		if (navswitch_push_event_p (NAVSWITCH_SOUTH))
+			goSouth(&player1, 1);
 
 		if (navswitch_push_event_p (NAVSWITCH_EAST))
-      goEast(&player1, 1);
+			goEast(&player1, 1);
 
-    if (navswitch_push_event_p (NAVSWITCH_WEST))
-      goWest(&player1, 1);
+		if (navswitch_push_event_p (NAVSWITCH_WEST))
+			goWest(&player1, 1);
 
 
+		//triggers the LED to indicate whether a bomb availiable
 		if (player1.hasBomb == 1){
 			pio_output_high (LED_PIO);
 		}else{
 			pio_output_low (LED_PIO);
 		}
 
-    if (navswitch_push_event_p (NAVSWITCH_PUSH)){
+		//Listener for the navswitch
+		//when pressed, place a bomb is one is possible
+		if (navswitch_push_event_p (NAVSWITCH_PUSH)){
 
-		  if (player1.hasBomb == 1){
-          placeBomb(&player1, 1);
+			if (player1.hasBomb == 1){
+					placeBomb(&player1, 1);
 			}
-			player1.hasBomb = 0;
+			player1.hasBomb = 0; //resets bomb
 		}
 
-        //revieve???
-   if(ir_uart_read_ready_p ()){
 
+	//checks whether there is something to recieve
+	if(ir_uart_read_ready_p ()){
+			//recieves character
 			char character = ir_uart_getc ();
-      switch(character){
-    			case 'n':
-       				goSouth(&player2, 0);
-              break;
-    			case 's'  :
-      				goNorth(&player2, 0);
-       				break;
-    			case 'e'  :
-      				goWest(&player2, 0);
-       				break;
-       		case 'w'  :
-      				goEast(&player2, 0);
-       				break;
-       		case 'b'  :
-      				placeBomb(&player2, 0);
-       				break;
-          case 'W' :
-              win = 1;
-              break;
-          case 'L' :
-              lose = 1;
-              break;
-       		default:
-       				break;
+
+			//switch statement to assign action based upon char recieved.
+			//calls the required directional functions and does not transmit to the other MCU.
+
+			//These recieved characters are used to update the local version of player2's location based ...
+			//upon the actions of the other MCU.
+			switch(character){
+					case 'n':
+							goSouth(&player2, 0);
+							break;
+					case 's'  :
+							goNorth(&player2, 0);
+							break;
+					case 'e'  :
+							goWest(&player2, 0);
+							break;
+					case 'w'  :
+							goEast(&player2, 0);
+							break;
+					case 'b'  :
+							placeBomb(&player2, 0);
+							break;
+					case 'W' :
+							win = 1; //win
+							break;
+					case 'L' :
+							lose = 1; //lose
+							break;
+					default:
+							break;
 			}
 		}
 
 
 
 
-    }
+		}
 
 
-    if (win > 0){
-      showWinScreen();
-    }else{
-      showLoseScreen();
-    }
+		//after game is finished (loop breaks after explosion), a win or lose screen is displayed
+		if (win > 0){
+			showWinScreen();
+		}else{
+			showLoseScreen();
+		}
 
 
-    return 0;
+		return 0;
 }
